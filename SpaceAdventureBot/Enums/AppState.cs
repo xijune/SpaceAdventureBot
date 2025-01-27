c54129512da846d229ced4c10f2e3d0aff605d79
Stefan <stefan.petrovic@gerber-systems.ch>
2025-01-27 11:03:26 +0100

    Changed name of the project to SpaceAdventureBot

---
+++ SpaceAdventureBot/Enums/AppState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceAdventureBot.Enums
+{
+    public enum ApplicationState
+    {
+        Started,         // Application is running
+        FailedToStart,   // Application failed to start
+        Crashed,         // Application crashed after launching
+        Terminated,      // Application was running but is now closed
+        Unknown          // Unknown state
+    }
+}
